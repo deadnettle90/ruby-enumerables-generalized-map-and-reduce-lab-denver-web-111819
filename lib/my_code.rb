cb8279910637bbc152c1e86309d3bdef4a8d60c8
Danielle Matton <danimatton@gmail.com>
2019-10-31 03:27:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,13 +11,13 @@ end
 
 def reduce(array, value=nil)
   if value 
-    num1 = value
+    sum = value
     i = 0 
-  else num1 = array[0]
+  else sum = array[0]
     i = 1
 end
 while i < array.length do
-  yield(num1, array[i])
+  sum = yield(sum, array[i])
   i += 1
 end
 
